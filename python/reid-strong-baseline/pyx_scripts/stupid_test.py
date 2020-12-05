fea826535d26356b16774aa693709eefe8c0fe04
HoEmpire <541218251@qq.com>
2020-12-05 10:50:19 +0800

    save the files before things corrupt

--- python/reid-strong-baseline/pyx_scripts/stupid_test.py
+++ python/reid-strong-baseline/pyx_scripts/stupid_test.py
@@ -1,21 +1,24 @@
 import os
 import sys
+
 sys.path.append('.')
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from config import cfg
+from cv_bridge import CvBridge
 from data import make_data_loader
 from engine.inference import inference
 from modeling import baseline
-
-import matplotlib.pyplot as plt
-from torchvision import transforms
 from PIL import Image
-import time
-import torch
+from ptl_msgs.msg import ImageBlock
+from torchvision import transforms
 from utils import reid_metric
-import numpy as np
 
 
-def main():
+def build_model():
     num_classes = 1
     last_stride = 1
     pretrain_path = "/home/tim/market_resnet50_model_120_rank1_945.pth"
@@ -29,6 +32,82 @@ def main():
     model.load_param(pretrain_path)
     model.eval()
     model.cuda()
+    return model
+
+
+def blur_detection(img):
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = cv2.Laplacian(img_gray, cv2.CV_64F, ksize=1)
+    score = res.var()
+    if score < 100.0:
+        rospy.info(
+            "Image Blur Detected, discard this image for database maintaince!")
+        return True
+    else:
+        return False
+
+
+class Object():
+    def __init__(self):
+        self.id = id
+        self.feats = feat
+
+
+    def add_feat(self, feat):
+        feat.feats = torch.cat((self.feats, feat), 0)
+
+
+class ReIDDatabase():
+    def __init__(self):
+        self.object_list = []
+        self.feat_id_list = []
+        self.feat_all = torch.tensor([])
+        self.object_num = 0
+
+    def init_database(self, feats):
+        for f in feats:
+            new_object = Object(self.object_num, f)
+            self.object_list.append(new_object)
+            self.feat_id_list.append(new_object.id)
+            if self.feat_all.shape[0] == 0:
+                self.feat_all = f
+            else:
+                self.feat_all = torch.cat((self.feat_all, f), 0)
+            self.object_num = self.object_num++
+            
+
+    def query_feat(self, feats):
+        if self.id_max == 0:
+            self.init_database(feats)
+        else:
+            distmat = reid_metric.re_ranking(qf, gf, k1=20, k2=6, lambda_value=0.5)
+            print(distmat.shape)
+            rank = np.argsort(distmat, axis=1).squeeze()
+
+            for i in range(10):
+                print(gallery_list[rank[i]])
+                print(distmat.squeeze(0)[rank[i]])
+                plt.figure(0)
+                plt.imshow(image[rank[i]].numpy().transpose(1, 2, 0))
+                plt.show()
+             
+
+
+class ReIDNode():
+    def __init__(self):
+        self.model = build_model()
+        rospy.info("Load ReID net successfully!")
+
+    def tracker_info_callback(self, data):
+        bridge = CvBridge()
+        cvimg = bridge.imgmsg_to_cv2(data.img, "bgr8")
+    
+    def re_identification(self, data)
+
+
+def main():
+    model = build_model()
 
     gallery_root = "/home/tim/ptl_project/src/pedestrain_tracking_and_localizaiton/python/reid-strong-baseline/data/test_data/gallery2"
     query_root = "/home/tim/ptl_project/src/pedestrain_tracking_and_localizaiton/python/reid-strong-baseline/data/test_data/query"
